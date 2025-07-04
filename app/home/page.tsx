722a93bd98e1c1f63740f8a92b99635ae6bf99bf
Francesco Zuppichini <francesco.zuppichini@gmail.com>
2025-07-04 18:19:25 +0200

    admin topbar

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,26 +1,12 @@
 import { AppGrid } from "@/components/phone/app-grid";
-import { PhoneContainer } from "@/components/phone/phone-container";
 import { StatusBar } from "@/components/phone/status-bar";
-import { redirect } from "next/navigation";
-import { auth } from "@/lib/auth";
 
 export const runtime = "edge";
 
-export default async function HomePage() {
-  const session = await auth();
-
-  if (!session) {
-    redirect("/");
-  }
-
+export default function HomePage() {
   return (
-    <main className="grid min-h-screen flex-col ">
+    <main className="grid h-full flex-col">
       <div className="flex flex-col h-full">
-        {session?.user?.admin && (
-          <p className="p-2 flex items-center justify-center">
-            YOU ARE AN ADMIN for {session?.user.name}
-          </p>
-        )}
         <StatusBar />
         <AppGrid />
       </div>
